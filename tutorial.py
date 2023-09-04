b5d2eb9309d524d5cfa1d728380224cd35a9622e
fjodoin <fjodoin@protonmail.com>
2023-09-04 17:55:08 -0400

    Player sprite and horizontal movement functional

--- tutorial.py
+++ tutorial.py
@@ -15,8 +15,38 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    # For loop to load individual sprite sheets, iterate through, and transform if necessary
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha() # Load an image from the available sprites, and add a transparent background
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0 , width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)           
+            sprites.append(pygame.transform.scale2x(surface))       # x2 size of sprites
+        
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+    
+    return all_sprites
+
 class Player(pygame.sprite.Sprite):         # pygame.sprite.Sprite allows for easy collision perfect detection
     COLOR = (255, 0, 0)
+    GRAVITY = 0
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)    # Spawn player sprite!
+
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
@@ -24,6 +54,7 @@ class Player(pygame.sprite.Sprite):         # pygame.sprite.Sprite allows for ea
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -43,10 +74,14 @@ class Player(pygame.sprite.Sprite):         # pygame.sprite.Sprite allows for ea
     
     def loop(self, fps):
         # Loop will be called once every frame and perform animation (moving, jumping, etc.)
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)        # using "min with 1" so that gravity kicks in at 0 seconds, instead of 1 second.
         self.move(self.x_vel, self.y_vel)
+        
+        self.fall_count += 1
 
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 # Create a backsplash for the background
 def get_background(name):
@@ -67,6 +102,15 @@ def draw(window, background, bg_image, player):
     player.draw(window)
     pygame.display.update()
 
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
 def main(window):
     # Event loops
     clock = pygame.time.Clock()
@@ -80,6 +124,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        player.loop(FPS)
+        handle_move(player)
         draw(window, background, bg_image, player)
     
     pygame.quit()
